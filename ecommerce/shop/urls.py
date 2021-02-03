36b827cef168a14dee8e45c8f8b392a90bd56f65
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2021-02-03 13:03:58 +0530

    Stripe Integration - part 1

--- ecommerce/shop/urls.py
+++ ecommerce/shop/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('cart/', CartListView.as_view(), name='shop_cart_home'),
     path('cart/add/<id>/', add_to_cart, name='shop_add_cart'),
+
+    path('create-checkout-session/', create_checkout_token, name='create_checkout_token')
 ]
--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -1,8 +1,14 @@
+from django.http.response import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView, ListView
 from .models import *
 from product.models import Product
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.conf import settings
+import stripe
+import json
 
 # Create your views here.
 @login_required
@@ -23,3 +29,39 @@ class CartListView(ListView):
         queryset = super(CartListView, self).get_queryset()
         queryset = queryset.filter(user=self.request.user).select_related('product')
         return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        total = 0
+        cart_items = Cart.objects.filter(user=self.request.user).select_related('product')
+        for i in cart_items:
+            total += i.product.price
+        context['total'] = total
+        context['stripe_publishable_key'] = settings.STRIPE_PUBLISHABLE_KEY
+        return context
+
+@csrf_exempt
+# @require_POST
+def create_checkout_token(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    checkout_session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[
+            {
+                'price_data': {
+                    'currency': 'inr',
+                    'unit_amount': 2000,
+                    'product_data': {
+                        'name': 'Stubborn Attachments',
+                        'images': ['https://i.imgur.com/EHyR2nP.png'],
+                    },
+                },
+                'quantity': 1,
+            },
+        ],
+        mode='payment',
+        success_url='https://testsite.com/success.html',
+        cancel_url='https://testsite.com/cancel.html',
+    )
+
+    return JsonResponse(checkout_session)
